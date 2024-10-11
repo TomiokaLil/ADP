daba24f4807267d76652e46d01c40d42d86609ad
ToMy18 <djfromix9@gmail.com>
2024-10-10 22:23:31 -0600

    se añade archivo de actualizar con mensaje de bienvenida

---
+++ test.py
@@ -0,0 +1,14 @@
+pasos = "paso 1 tener python" 
+descripcion = "sirve para actualizar perfiles"
+autor = "Rodrigo Gc"
+fecha = "10/10/24"
+licencia = "MIT license"
+
+def mensajebienvenida():
+    print(descripcion)
+    print(autor)
+    print(fecha)
+    print(licencia)
+    print(pasos)
+
+mensajebienvenida()
